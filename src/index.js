999e3d182c4962e70317b684f884ccf03f8fea79
finnshort <finn.d.short@gmail.com>
2017-07-07 12:24:41 -0700

    lifted state to farm component-beds can render options

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Bed extends React.Component {
   render() {
     return (
-      <button className="bed" onClick={() => this.props.onClick()}>
+      <button className={"bed " + this.props.crop} onClick={() => this.props.onClick()}>
         {this.props.crop}
       </button>
     );
@@ -24,23 +24,11 @@ class Option extends React.Component {
 }
 
 class Garden extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      beds: Array(9).fill(null),
-    };
-  }
-
-  handleClick(i) {
-    const beds = this.state.beds.slice();
-    beds[i] = 'kale';
-    this.setState({beds: beds});
-  }
 
   renderBed(i) {
     return <Bed
-      crop={this.state.beds[i]}
-      onClick={() => this.handleClick(i)}
+      crop={this.props.beds[i]}
+      onClick={() => this.props.onClick(i)}
       />;
   }
 
@@ -70,25 +58,13 @@ class Garden extends React.Component {
 }
 
 class ControlPanel extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      cropOptions: ["carrots", "kale", "peas"],
-      selectedCropIndex: 0
-    };
-  }
 
   renderOption(i) {
     return <Option
-      option={this.state.cropOptions[i]}
-      isActive={this.state.selectedCropIndex === i}
-      onClick={() => this.handleClick(i)}
+      option={this.props.cropOptions[i]}
+      isActive={this.props.selectedCropIndex === i}
+      onClick={() => this.props.onClick(i)}
       />;
-  }
-
-  handleClick(i) {
-    // const clickedCrop = this.state.cropOptions[i];
-    this.setState({selectedCropIndex: i});
   }
 
   render(){
@@ -98,9 +74,9 @@ class ControlPanel extends React.Component {
       <div>
         <div className="instructions">{instructions}</div>
         <ul className="cropList">
-        <li>{this.renderOption(0)}</li>
-        <li>{this.renderOption(1)}</li>
-        <li>{this.renderOption(2)}</li>
+          <li>{this.renderOption(0)}</li>
+          <li>{this.renderOption(1)}</li>
+          <li>{this.renderOption(2)}</li>
         </ul>
       </div>
     )
@@ -108,14 +84,38 @@ class ControlPanel extends React.Component {
 }
 
 class Farm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      beds: Array(9).fill(null),
+      cropOptions: ["carrots", "kale", "peas"],
+      selectedCropIndex: 0
+    };
+  }
+  handleBedClick(i) {
+    const beds = this.state.beds.slice();
+    const index = this.state.selectedCropIndex;
+    beds[i] = this.state.cropOptions[index];
+    this.setState({beds: beds});
+  }
+  handleOptionClick(i) {
+    this.setState({selectedCropIndex: i});
+  }
   render() {
     return (
       <div className="farm">
         <div className="control-panel">
-          <ControlPanel />
+          <ControlPanel
+            cropOptions={this.state.cropOptions}
+            selectedCropIndex={this.state.selectedCropIndex}
+            onClick={(i) => this.handleOptionClick(i)}
+            />
         </div>
         <div className="farm-garden">
-          <Garden />
+          <Garden
+            beds={this.state.beds}
+            onClick={(i) => this.handleBedClick(i)}
+            />
         </div>
         <div className="farm-info">
           <div>{/* status */}</div>
